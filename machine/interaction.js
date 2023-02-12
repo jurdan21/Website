fc1b49fa3e2bf34c989da9e7419009f04d67894e
Creeping Machine <96480222+mesinmerayap@users.noreply.github.com>
2023-02-12 22:09:10 +0700

    Pembongkaran

--- machine/interaction.js
+++
@@ -1,6 +0,0 @@
-const splash = document.querySelector ('.intro');
-document.addEventListener ('DOMContentLoaded', (e)=>{
-    setTimeout (()=> {
-        splash.classList.add('display-none');
-    }, 1200);
-})
--- machine/trigger.js
+++
@@ -1,9 +0,0 @@
-function navbarmenuOpen() {
-    var kalcer = document.getElementById("navbarTrigger");
-    kalcer.style.display = "flex";
-}
-
-function navbarmenuClose() {
-    var skena = document.getElementById("navbarTrigger");
-    skena.style.display = "none";
-}
