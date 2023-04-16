6a7049804d562f277a522b484d2341953b154b0e
Creeping Machine <96480222+mesinmerayap@users.noreply.github.com>
2023-04-16 13:11:55 +0700

    Update intro dll lah pokoknya anjengg

--- machine/trigger.js
+++ machine/trigger.js
@@ -13,3 +13,13 @@ const teks2 = document.querySelector(".headerExpand");
 tombol2.addEventListener("click", function() {
   teks2.style.display = "none";
 });
+
+
+//trigger intro
+const myDiv = document.getElementById('intro-trigger');
+const myClass = document.querySelector('.introMawar');
+
+// Mengubah nilai background-color setelah 3 detik
+setTimeout(() => {
+  myClass.classList.add('hide');
+}, 1000);
